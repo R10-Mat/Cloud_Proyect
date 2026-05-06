e588a6a278fd37623cad78d4e9adab7ad0070124
R10-Mat <rayhan.matos@utec.edu.pe>
2026-05-06 15:25:44 -0500

    Sin Front

--- Frontend/src/api/orquestadorApi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const ORQUESTADOR = axios.create({ baseURL: "http://localhost:8001" });
-
-export const getResumen = () => ORQUESTADOR.get("/dashboard/resumen");
-
-export const getDetalleEnvio = (pedidoId) =>
-  ORQUESTADOR.get(`/dashboard/envio/${pedidoId}`);
